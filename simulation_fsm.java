2f164bc891cfd6a92f10fd34053d116577a7b12c
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-10-31 18:04:48 +0530

    Updated

--- simulation_fsm.java
+++ simulation_fsm.java
@@ -28,3 +28,44 @@ public class fsm {
  
 }
  
+/*
+package compiler;
+import java.util.*;
+public class fsm {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Map<String,String> hash = new HashMap();
+		
+		hash.put("deposit", "WAITING");
+		hash.put("quit", "EXIT");
+		hash.put("select","DISPENSE");
+		hash.put("refund","REFUNDING");
+		hash.put("remove", "READY");
+		
+		String questions[] = {"[deposit,quit]","[select,refund]","[deposit,quit]","[select,refund]","[select,refund]","[remove]","[deposit,quit]"};
+		
+		Map<String,String> hashone = new HashMap();
+		hashone.put("WAITING",questions[1]);
+		hashone.put("EXIT","END");
+		hashone.put("DISPENSE",questions[5]);
+		hashone.put("REFUNDING",questions[0]);
+		hashone.put("READY",questions[0]);
+		String answers[] = new String[questions.length-1];
+		
+		System.out.println("READY");
+		System.out.println(hashone.get("READY"));
+		while()
+		{
+			String answer = sc.next();
+			System.out.println(hash.get(answer));
+			System.out.println(hashone.get(hash.get(answer)));
+		}
+		
+		
+		
+	}
+ 
+}
+ 
+*/
